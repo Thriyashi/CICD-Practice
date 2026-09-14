4b08312f75b9c7d54b0d571e5e8e80c2111e9292
Thriyashi <thriyashiradika@gmail.com>
2026-09-14 10:30:20 +0530

    Remove insecure code

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-import subprocess
+
 
 def add(a, b):
     return a + b
 
 print("CI/CD practice app is running")
 
-# Intentionally insecure code for testing Bandit
-subprocess.call("dir", shell=True)
