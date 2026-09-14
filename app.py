27e833ccce54b34431e6fa86e8e348146c666078
Thriyashi <thriyashiradika@gmail.com>
2026-09-14 10:23:32 +0530

    Check Bandit Security Scanning

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
+import subprocess
+
 def add(a, b):
-   return a + b
+    return a + b
 
 print("CI/CD practice app is running")
+
+# Intentionally insecure code for testing Bandit
+subprocess.call("dir", shell=True)
